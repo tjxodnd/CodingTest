85d0cc5f5c4f59a27fd124adeb21fa42bd87db03
stw <eru962naver.com>
2021-06-15 14:20:34 +0900

    11 solve!

--- TestStringSection.java
+++ TestStringSection.java
@@ -243,5 +243,68 @@ class TestStringSection {
 		}
  		return answerList;
 	}
+	
+
+	//11. 문자열 압축
+	//문제 : 압축된 문자열 출력
+	//input : KKHSSSSSSSE 
+	//output : K2HS7E
+	public static void main(String[] args) {
+		TestStringSection T = new TestStringSection();
+		Scanner kb = new Scanner(System.in);
+		String str = kb.nextLine();
+		System.out.println(T.solution(str)); 
+	}
+
+	public String solution(String s) {
+		String answer = "";
+		s = s+" ";
+		int cnt = 1;
+		
+		for(int i = 0; i < s.length()-1; i++) {
+			if(s.charAt(i)==s.charAt(i+1)){
+				cnt++;
+			} else {
+				answer+=s.charAt(i);
+				if(cnt>1) {
+					answer+=String.valueOf(cnt);
+				}
+				cnt=1;
+			}
+		}
+
+ 		return answer;
+	}
 	*/
+
+	//12. 암호
+	//문제 : 압축된 문자열 출력
+	//input : KKHSSSSSSSE 
+	//output : K2HS7E
+	public static void main(String[] args) {
+		TestStringSection T = new TestStringSection();
+		Scanner kb = new Scanner(System.in);
+		String str = kb.nextLine();
+		System.out.println(T.solution(str)); 
+	}
+
+	public String solution(String s) {
+		String answer = "";
+		s = s+" ";
+		int cnt = 1;
+		
+		for(int i = 0; i < s.length()-1; i++) {
+			if(s.charAt(i)==s.charAt(i+1)){
+				cnt++;
+			} else {
+				answer+=s.charAt(i);
+				if(cnt>1) {
+					answer+=String.valueOf(cnt);
+				}
+				cnt=1;
+			}
+		}
+
+ 		return answer;
+	}
 }
