e392f63bce4d5b8a72059160de642525999dea28
twseo <eru96@naver.com>
2022-03-17 23:23:23 +0900

    2022.03.17 커밋

--- TestPointerSection.java
+++ TestPointerSection.java
@@ -62,8 +62,8 @@ public class TestPointerSection {
         }
 
         Arrays.sort(c);
-        for(int cs : c) {
-            System.out.println(cs);
+        for(int css : c) {
+            System.out.println(css);
         }
     }
 
